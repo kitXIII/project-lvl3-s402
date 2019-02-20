a462b9d018e4750a0abd711f4b412b7f908d4961
Nikita <mipo4t@gmail.com>
2019-02-20 22:02:54 +0400

    add parser

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,13 +1,20 @@
 import axios from './axios';
-import isRssMimiType from './utils';
+import isRssMimeType from './utils';
+import parse from './parser';
 
 export default url => axios.get(url, {
   validateStatus: status => status === 200,
   timeout: 10000,
 })
   .then((resp) => {
-    if (!isRssMimiType(resp)) {
-      throw new Error('It is not rss address response');
+    if (!isRssMimeType(resp)) {
+      throw new Error('It is not rss address');
     }
-    return resp;
+    if (!resp.data) {
+      throw new Error('Response data is empty');
+    }
+    const domParser = new DOMParser();
+    const xml = domParser.parseFromString(resp.data, 'text/xml');
+    const channel = xml.querySelector('channel');
+    return parse(channel.children);
   });
---
+++ src/js/parser.js
@@ -0,0 +1,24 @@
+import { keys } from 'lodash';
+
+const parse = (nodes) => {
+  const items = [];
+  const transform = {
+    item: (node, acc) => {
+      items.push(parse(node.children));
+      return acc;
+    },
+    title: (node, acc) => ({ ...acc, title: node.textContent }),
+    description: (node, acc) => ({ ...acc, description: node.textContent }),
+    link: (node, acc) => ({ ...acc, description: node.textContent }),
+  };
+
+  const result = [...nodes].filter(node => keys(transform).includes(node.tagName))
+    .reduce((acc, node) => transform[node.tagName](node, acc), {});
+
+  if (items.length) {
+    return { ...result, items };
+  }
+  return result;
+};
+
+export default parse;
