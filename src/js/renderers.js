61eff2fd2313fdd5a2e0d954ee213c0425bb36cb
Nikita <mipo4t@gmail.com>
2019-02-23 21:20:48 +0400

    add update feeds

--- src/js/renderers.js
+++ src/js/renderers.js
@@ -1,18 +1,28 @@
-import { flatten } from 'lodash';
+import { flatten, sortBy } from 'lodash';
 
-export const renderFeedTitles = feeds => feeds.map(feed => `
-  <div class="pl-2">
-    <h5>${feed.title}</h5>
-    <p class="font-weight-light pl-1">${feed.description}</p>
-  </div><hr/>`).join('');
+export const renderFeedTitles = feeds => sortBy(feeds, feed => Date.parse(feed.pubDate))
+  .reverse()
+  .map(feed => `
+    <div class="pl-2">
+      <h5>${feed.title}</h5>
+      <p class="font-weight-light pl-1">${feed.description}</p>
+    </div><hr/>`)
+  .join('');
 
 export const renderFeedItems = (feeds, itemsModalElementId) => {
-  const renderedParts = feeds.map((feed, feedIndex) => feed.items.map((item, itemIndex) => `
-    <div class="d-flex justify-content-between align-items-center py-1">
-      <a href="${item.link}">${item.title}</a>
-      <button type="button" class="btn btn-sm btn-secondary ml-2" data-toggle="modal" data-target="#${itemsModalElementId}" data-channel="${feedIndex}" data-index="${itemIndex}">
-        view
-      </button>
-    </div>`));
-  return flatten(renderedParts).join('');
+  const itemsWithRenders = flatten(feeds
+    .map((feed, feedIndex) => feed.items.map((item, itemIndex) => ({
+      ...item,
+      render: `
+        <div class="d-flex justify-content-between align-items-center py-1">
+          <a href="${item.link}">${item.title}</a>
+          <button type="button" class="btn btn-sm btn-secondary ml-2" data-toggle="modal" data-target="#${itemsModalElementId}" data-channel="${feedIndex}" data-index="${itemIndex}">
+            view
+          </button>
+        </div>`,
+    }))));
+  return sortBy(itemsWithRenders, item => Date.parse(item.pubDate))
+    .reverse()
+    .map(item => item.render)
+    .join('');
 };
